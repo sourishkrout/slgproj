d04b3352b619a07c978b9c54698fcd6be2955fc6
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2012-02-25 21:31:09 -0800

    Getting closer.

--- gallery.js
+++ gallery.js
@@ -14,19 +14,25 @@ krout.gallery.prototype.init = function(jsonurl) {
 			_this.data = data;
 			
 			_this._domElements();
-			_this._createThumbs();
+			_this._createAlbum();
 			
-			_this.select(3);
+			_this.select(1);
 		}
 	});
 };
 
-krout.gallery.prototype._createThumbs = function() {
+krout.gallery.prototype._createAlbum = function() {
 	var _this = this;
+	var last = -1;
+	
+	var title = $(this.div).find('div.title');
+		title.text(this.data.album.name);
+	
 	var thumbs = $(this.div).find('div.thumbs');
 	for (var i=0; i < this.data.photos.length; i++) {
 		var t = this.data.photos[i];
-		thumbs.append('<img class="thumb" src="' + t.thumb_url + '" gallery:idx="' + i + '" />')
+		var id = t.id;
+		thumbs.append('<img class="thumb" src="' + t.thumb_url + '" gallery:idx="' + t.id + '" />')
 	}
 	
 	$(thumbs).find('img').each(function(idx, item) {
@@ -35,27 +41,73 @@ krout.gallery.prototype._createThumbs = function() {
 			_this.select(parseInt(photo));
 		});
 	});
+	
+	$(this.div).find('div.prev a').click(function(e) {
+		_this.select(_this._prevIdx());
+	});
+
+	$(this.div).find('div.next a').click(function(e) {
+		_this.select(_this._nextIdx());
+	});
 };
 
 krout.gallery.prototype._domElements = function() {
-	$(this.div).append('<div class="header"></div><div class="body"><img class="photo" /><div class="legend"><div class="caption">Jessica and Amy by the lake</div><div class="location">Taken February 26, 2008 in Paris, France</div></div></div><div class="thumbs"></div>');
+	var html = '<div class="header">';
+	html += '<div class="prev"><a href="javascript:void(0);"><img src="button_prev.png" /></a></div>';
+	html += '<div class="title">Title</div>';
+	html += '<div class="next"><a href="javascript:void(0);"><img src="button_next.png" /></a></div>';
+	html += '</div>';
+	html += '<div class="body"><img class="photo" />';
+	html += '<div class="legend"><div class="caption">Caption</div>';
+	html += '<div class="location">Location</div></div></div>';
+	html += '<div class="thumbs"></div>';
+	$(this.div).append(html);
 	this.photo = $(this.div).find('img.photo');
 };
 
 krout.gallery.prototype.select = function(idx) {
-	if (idx < 0 || this.data.photos.length <= idx)
-		return;
+	var img = this.data.photos[0];
+	
+	for (var i=0; i < this.data.photos.length; i++) {
+		if (parseInt(this.data.photos[i].id) == parseInt(idx)) {
+			img = this.data.photos[i];
+			break;
+		} 
+	}
+	
+	var location = function() {
+		return 'Taken on ' + img.date + ' in ' + img.location;
+	};
 		
-	var img = this.data.photos[idx];
-	this.idx = idx;
-	
 	$(this.photo).attr('src', img.url);
+	$(this.div).find('div.caption').text(img.title);
+	$(this.div).find('div.location').text(location());
 	
-	// Reset them all.
 	var thumbs = $(this.div).find('div.thumbs');
 	$(thumbs).find('img').each(function(idx, itme) {
 		$(this).addClass('selected');
 	});
+	
+	this.idx = parseInt(img.id);
+	return img.id;
+};
+
+krout.gallery.prototype._nextIdx = function() {
+	for (var i=0; i < this.data.photos.length; i++) {
+		if (parseInt(this.idx) == parseInt(this.data.photos[i].id) && this.data.photos[i+1] != null)
+			return parseInt(this.data.photos[i+1].id);
+	}
+	
+	return 0;
+};
+
+krout.gallery.prototype._prevIdx = function() {
+	for (var i=0; i < this.data.photos.length; i++) {
+		if (parseInt(this.idx) == parseInt(this.data.photos[i].id) && this.data.photos[i-1] != null)
+			return parseInt(this.data.photos[i-1].id);
+	}
+
+	return this.data.photos[this.data.photos.length-1].id;
 };
 
 $(function() {
